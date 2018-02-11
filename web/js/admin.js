2db555da9cb23ffcb91918e7e792ea8dc8f11209
DJ-Web-design <33030242+DJ-Web-design@users.noreply.github.com>
2018-02-11 15:08:42 -0400

    Update admin.js

--- web/js/admin.js
+++ web/js/admin.js
@@ -38,8 +38,7 @@ var app = new Vue({
 
             if (fileTitle && fileDes && fileGal && this.file){
                 let sendFile = this.file[0];
-
-                var upload = storageChild.child(this.sendFile.name).put(sendFile)
+                var upload = storageChild.child(fileTitle).put(sendFile);
 
                 upload.on("state_changed", snapshot => {
                     let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
