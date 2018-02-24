f050c1da79dbdb9384c6ed8345d4efeef962258a
DJ-Web-design <DJ-Web-design@github.com>
2018-02-23 20:08:48 -0400

    Arreglada animacion del menu movil

--- web/js/menu.js
+++ web/js/menu.js
@@ -1,1 +1,64 @@
-$(document).ready(main, sombra);var contador = 1;var contador2 = 1;function main(){$('.menu-cel').click(()=>{if(contador == 1){$('nav').animate({left: '0',});contador = 0;} else {contador = 1;$('nav').animate({left: '-100%'});}});$('.menu-cel').click(()=>{if(contador2 == 1){$('div.sombra').css({display: 'block',right: '0',transition: '0.2s'});contador2 = 0;} else {contador2 = 1;$('div.sombra').css({display: 'none'});}});$('.submenu').click(()=> {$(this).children('.children').slideToggle();});};function sombra(){$(".menu-cel").click(function (event) {if (contador == 1) {$(".sombra").fadeIn(100)} else {$(".sombra").fadeOut(100)}});$(".sombra").click(function (event) {if (contador == 0) {contador = 1;$(".sombra").fadeOut(100);$("nav").animate({ left: "-100%" });}})};
+$(document).ready(main);
+var contador = 1;
+var contador2 = 1;
+function main() {
+    $('.menu-cel').click(() => {
+        if(contador == 1) {
+            $('nav').animate({
+                left: '0',
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('nav').animate({
+        left: '-100%'
+      });
+        }
+    });
+    $('.menu-cel').click(() => {
+        if(contador2 == 1) {
+            $('div.sombra').css({
+                display: 'block',
+                right: '0',
+                transition: '0.2s'
+            });
+            contador2 = 0;
+        } else {
+            contador2 = 1;
+            $('div.sombra').css({
+        display: 'none'
+      });
+        }
+    });
+    $('.sombra').click(() => {
+        if (contador == 1) {
+            $('nav').animate({
+                left: '0',
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('nav').animate({
+                left: '-100%'
+            });
+        }
+    });
+    $('.sombra').click(() => {
+        if (contador2 == 1) {
+            $('div.sombra').css({
+                display: 'block',
+                right: '0',
+                transition: '0.2s'
+            });
+            contador2 = 0;
+        } else {
+            contador2 = 1;
+            $('div.sombra').css({
+                display: 'none'
+            });
+        }
+    });
+    $('.submenu').click(() => {
+        $(this).children('.children').slideToggle();
+    });
+};
