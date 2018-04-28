4d2eb4e29e42dfca439a2b7fe49201eea053ae93
DJ-Web-design <DJ-Web-design@github.com>
2018-04-28 14:11:42 -0400

    Prueba BBDD

--- web/votos.php
+++ web/votos.php
@@ -1,33 +1,26 @@
 <?php
 	require("../config.php"); 
 	
-
     $pedido1 = isset($_GET["voto1"]) ? $_GET["voto1"]:$_POST["voto1"];
     $pedido2 = isset($_GET["voto2"]) ? $_GET["voto2"]:$_POST["voto2"];
 	$pedido3 = isset($_GET["voto3"]) ? $_GET["voto3"]:$_POST["voto3"];
 
 	$dbconn = pg_connect("host=".$host." port=".$port." dbname=".$db." user=".$user." password=".$pass) 
 		or die('NO HAY CONEXION: ' . pg_last_error());
-
-
-		$query = "SELECT * FROM vota WHERE vota LIKE '$pedido1'";
-		$result = pg_query($dbconn, $query) or die('ERROR AL PEDIR DATOS: ' . pg_last_error());
-		$voto1 = pg_affected_rows($result);
-		echo $voto1;
 	
-    /*if (isset($pedido1) && isset($pedido2) && isset($pedido3)) {
+    if (isset($pedido1) && isset($pedido2) && isset($pedido3)) {
 
 		$query = "SELECT * FROM vota WHERE vota LIKE '$pedido1'";
     	$result = pg_query($dbconn, $query) or die('ERROR AL PEDIR DATOS: ' . pg_last_error());
-		$voto1 = pg_num_row($result);
+		$voto1 = pg_affected_rows($result);
 
 		$query = "SELECT * FROM vota WHERE vota LIKE '$pedido2'";
     	$result = pg_query($dbconn, $query) or die('ERROR AL PEDIR DATOS: ' . pg_last_error());
-		$voto2 = pg_num_row($result);
+		$voto2 = pg_affected_rows($result);
 
 		$query = "SELECT * FROM vota WHERE vota LIKE '$pedido3'";
     	$result = pg_query($dbconn, $query) or die('ERROR AL PEDIR DATOS: ' . pg_last_error());
-		$voto3 = pg_num_row($result);
+		$voto3 = pg_affected_rows($result);
 
 	};
     // Free resultset liberar los datos
@@ -35,12 +28,10 @@
 
     // Closing connection cerrar la conexión
     pg_close($dbconn);
-
-
     $mensaje = new stdClass();
 	$mensaje -> res1 = $voto1;
 	$mensaje -> res2 = $voto2;
 	$mensaje -> res3 = $voto3;
-    $json = json_encode($mensaje);
-
-    echo $json;*/
+	$json = json_encode($mensaje);
+	
+    echo $json;
