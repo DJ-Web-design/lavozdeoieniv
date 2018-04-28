b4b08d02a47fb35bb5a62cc0b9ad0fab120270a5
DJ-Web-design <DJ-Web-design@github.com>
2018-04-28 13:42:03 -0400

    Cambiada Config de BBDD

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
     $host="ec2-107-20-193-202.compute-1.amazonaws.com";
     $user="dljvmhgfebvgbi";
     $db="dfdkgkah1fmo9i";
+    $port = 5432;
     $pass="d47f0a48daa208f44f992d9a984884dfd4de807405bfabaa41f538152a513814";
     $uri="postgres://dljvmhgfebvgbi:d47f0a48daa208f44f992d9a984884dfd4de807405bfabaa41f538152a513814@ec2-107-20-193-202.compute-1.amazonaws.com:5432/dfdkgkah1fmo9i";
     $cli="heroku pg:psql postgresql-infinite-73855 --app lavozdeoieniv";
