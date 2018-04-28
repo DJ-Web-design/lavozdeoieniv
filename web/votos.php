fc390228fbff3f7238188554b07b3abeb83448d5
DJ-Web-design <DJ-Web-design@github.com>
2018-04-28 14:00:30 -0400

    Prueba BBDD

--- web/votos.php
+++ web/votos.php
@@ -8,19 +8,25 @@
 
 	$dbconn = pg_connect("host=".$host." port=".$port." dbname=".$db." user=".$user." password=".$pass) 
 		or die('NO HAY CONEXION: ' . pg_last_error());
-	
-    if (isset($pedido1) && isset($pedido2) && isset($pedido3)) {
+
 
 		$query = "SELECT * FROM vota WHERE vota LIKE '$pedido1'";
-    	$result = pg_query($dbconn, $query) or die('ERROR AL INSERTAR DATOS: ' . pg_last_error());
+		$result = pg_query($dbconn, $query) or die('ERROR AL PEDIR DATOS: ' . pg_last_error());
+		echo $result;
+		//$voto1 = pg_num_row($result);
+	
+    /*if (isset($pedido1) && isset($pedido2) && isset($pedido3)) {
+
+		$query = "SELECT * FROM vota WHERE vota LIKE '$pedido1'";
+    	$result = pg_query($dbconn, $query) or die('ERROR AL PEDIR DATOS: ' . pg_last_error());
 		$voto1 = pg_num_row($result);
 
 		$query = "SELECT * FROM vota WHERE vota LIKE '$pedido2'";
-    	$result = pg_query($dbconn, $query) or die('ERROR AL INSERTAR DATOS: ' . pg_last_error());
+    	$result = pg_query($dbconn, $query) or die('ERROR AL PEDIR DATOS: ' . pg_last_error());
 		$voto2 = pg_num_row($result);
 
 		$query = "SELECT * FROM vota WHERE vota LIKE '$pedido3'";
-    	$result = pg_query($dbconn, $query) or die('ERROR AL INSERTAR DATOS: ' . pg_last_error());
+    	$result = pg_query($dbconn, $query) or die('ERROR AL PEDIR DATOS: ' . pg_last_error());
 		$voto3 = pg_num_row($result);
 
 	};
@@ -37,4 +43,4 @@
 	$mensaje -> res3 = $voto3;
     $json = json_encode($mensaje);
 
-    echo $json;
+    echo $json;*/
