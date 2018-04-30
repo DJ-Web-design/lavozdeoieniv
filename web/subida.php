572a71a019ba4e474334e7720002ce64b1240116
DJ-Web-design <DJ-Web-design@github.com>
2018-04-30 19:45:41 -0400

    Prueba subida archivos

--- web/subida.php
+++ web/subida.php
@@ -8,7 +8,7 @@ if (isset($_FILES['archivo'])) {
    $extension = pathinfo($archivo['name'], PATHINFO_EXTENSION);
    $time = time();
    $nombre = "{$_POST['nombre_archivo']}_$time.$extension";
-   if (move_uploaded_file($archivo['tmp_name'], "archivos_subidos/$nombre")) {
+   if (move_uploaded_file($archivo['tmp_name'], "upload/$nombre")) {
       echo 1;
    } else {
       echo 0;
