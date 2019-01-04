757bae2b4f3f9c50defa1362b0bbb51b9b850bbf
Davids-Devel <djgm1206@gmail.com>
2019-01-04 14:57:55 -0400

    Fixed Upload

--- index.js
+++ index.js
@@ -1,97 +1,164 @@
-const express = require("express"),
-      app = express()
-      fileUpload = require('express-fileupload'),
-      {writeFileSync, unlinkSync, readFileSync} = require("fs"),
+//Server Const
+const express = require("express");
+const app = express();
+const fileUpload = require('express-fileupload');
 
-      require('es6-promise').polyfill(),
-      require("util").promisify();
-const fetch = require("isomorphic-fetch"),
-      UploadApi = require("./UploadVideo"),
+//Native Modules
+const {
+	existsSync,
+	writeFileSync,
+	readFileSync
+} = require("fs");
+const {join} = require("path");
 
-      PORT = process.env.PORT || 5000,
-      user = process.env.USER,
-      pass = process.env.PASS,
-      client_id = process.env.CLIENT_ID,
-      client_secret = process.env.CLIENT_SECRET
+//Fetch
+require('es6-promise').polyfill();
+const fetch = require("isomorphic-fetch");
+const UploadVideo = require("./UploadVideo");
 
-app.use(fileUpload())
-    .use((req, res, next)=>{
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    });
+//Env
+const PORT = process.env.PORT || 3000;
+const serverUser = process.env.USER;
+const serverPass = process.env.PASS;
+
 
 app
-    .get("/login-admin",(req, res) => {
-        let serverUser = req.query.user;
-        let serverPass = req.query.pass;
+	.use(fileUpload())
+	.use(express.json())
+	.use((req, res, next)=>{
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
 
-        if (serverUser === user && serverPass === pass) {
-            res.send("auth");
-        }else if (!serverUser || !serverPass){
-            res.status(403).send("<h1 style='text-align:center'>403</h1><hr/><p style='text-align:center'>Acceso no autorizado</p>");
-        }else {
-            res.send("no-auth");
-        }
-    })
-    .get("/youtube-auth", async ({query}, res)=>{
-        let code = query.code;
-        let dataToSend = `code=${code}&client_id=${client_id}&client_secret=${client_secret}&redirect_uri=https%3A%2F%2Flavozdeoieniv.herokuapp.com%2Fyoutube-auth&grant_type=authorization_code`;
-        try {
-        let response = await fetch("https://accounts.google.com/o/oauth2/token",{
-            method:"POST",
-            body:dataToSend,
-            headers:{
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        });
-        let data = await response.json();
+app
+	.get("/login-admin",({query}, res) => {
+		let {user, pass} = query;
 
-            access_token = data["access_token"];
-            refresh_token = data["refresh_token"];
+		if (serverUser === user && pass === serverPass)
+			res.send("auth");
+		else if (!user || !pass)
+			res.status(403).send("<h1 style='text-align:center'>403</h1><hr/><p style='text-align:center'>Acceso No Autorizado</p>");
+		else
+			res.send("no-auth");
+	})
+	.get("/authSuccess", async ({query}, res)=>{
+		//Auth Route
+		try {
+			let {code, authMethod} = query; //Google Account Code
+			let refresh_token = await UploadVideo.getRefreshToken(code); //Account Refresh Token
+			
+			if (!refresh_token) res.status(401).send("Error");
+			else {
+				//Save Refresh Token then redirect to upload route
+				if (authMethod === "youtube")
+					writeFileSync("temp/youtube-token.txt", refresh_token);
+				else if (authMethod === "blogger")
+					writeFileSync("temp/blogger-token.txt", refresh_token);
+				res.redirect(302, "/upload");
+			}
+		} catch(err) {
+			throw err;
+			res.status(500).send(err);
+		}
+	})
+	.post("/create-post",async ({body}, res)=>{
+		try {
+			let {content, title, labels} = body;
 
-            setTimeout(()=>{
-                access_token = undefined;
-                refresh_token = undefined;
-            }, data["expires_in"] * 1000);
+			let access_token = Buffer.from(readFileSync("temp/blogger-token.txt")).toString();
+		
+			let response = await fetch("https://www.googleapis.com/blogger/v3/blogs/8070105920543249955/posts/", {
+				method:"POST",
+				headers:{
+					"Authorization":access_token,
+					"Content-Type":"application/json"
+				},
+				body:JSON.stringify({
+					"kind": "blogger#post",
+					"blog": {
+						"id": "id"
+					},
+					"title": "A new post",
+					"content": "With <b>exciting</b> content..."
+				})
+			})
 
-            res.redirect(301, `https://www.lavozdeoieniv.tk/admin?access_token=${access_token}&refresh_token=${refresh_token}&acc=TGEgVm96IGRlIE9JRU5JVg==`);
+			let {url} = await response.json();
 
-        }catch(err){
-            res.status(err.status).send(err.statusText);
-        }
-    })
-    .post('/upload-video',(req,res) => {
-        let EDFile = req.files.file
-        var title = req.body.title;
-        var description = req.body.description;    
-        var access_token = req.body.access_token;
-        var refresh_token = req.body.refresh_token;
-        let mime;
-        switch (EDFile.mimetype) {
-            case "video/mp4":
-                mime = ".mp4";
-                break;
-            case "video/3gpp":
-                mime = ".3gp";
-                break;
-            case "video/x-msvideo":
-                mime = ".avi";
-                break;
-            default:
-                mime = ".mp4";
-                break;
-        }
-    
-        EDFile.mv(__dirname+"/tmp/video"+mime, async ()=>{
-            let API = new UploadApi();
-            API.setAccessRefreshToken(access_token, refresh_token);
+			res.json({
+				status:"success",
+				url
+			});
+		} catch(err) {
+			res.status(500).json({
+				status:"error",
+				error:err
+			})
+		}
+	})
+	.post("/edit-post",async({body}, res)=>{
+		try {
+			let access_token = Buffer.from(readFileSync("temp/blogger-token.txt")).toString();
+		
+			let response = await fetch("https://www.googleapis.com/blogger/v3/blogs/8070105920543249955/posts/3445355871727114160", {
+				method:"PATCH",
+				headers:{
+					"Authorization":access_token,
+					"Content-Type":"application/json"
+				},
+				body:JSON.stringify(body)
+			});
 
-            try {
-                res.send(await API.uploadVideo(title, description, __dirname+"/tmp/video"+mime)); 
-            } catch(err) {
-                res.status(500).send(err);
-            }
-        })
-    })
-    .listen(PORT, () => console.log("App preparada y escuchando en el puerto: " + PORT));
+			await response.json();
+
+			res.send("updated");
+		} catch(err) {
+			res.status(500).send("error");
+		}
+	})
+	.post('/upload-video', ({files, body},res) => {
+
+		let {file} = files;
+		var {title, description} = body;
+
+		let mime;
+		switch (file.mimetype) {
+			case "video/mp4":
+				mime = ".mp4";
+				break;
+			case "video/3gpp":
+				mime = ".3gp";
+				break;
+			case "video/x-msvideo":
+				mime = ".avi";
+				break;
+			default:
+				mime = ".mp4";
+				break;
+		}
+		var videoPath = join(__dirname, "temp", "video"+mime);
+		file.mv(videoPath, async ()=>{
+			try{
+				let fileBuffer = readFileSync(join(__dirname, "temp", "refresh-token.txt"));
+				let refresh_token = Buffer.from(fileBuffer).toString();
+
+				let access_token = await UploadVideo.getAccessToken(refresh_token);
+			
+				let Upload = new UploadVideo();
+
+				Upload.setAccessRefreshToken(access_token, refresh_token);
+				
+				let status = await Upload.uploadVideo(title, description, videoPath);
+				
+				res.json({
+					status
+				});
+			} catch(err) {
+				res.status(500).json({
+					status:err
+				});
+			}
+		});
+	})
+	.listen(PORT, () => console.log("App preparada y escuchando en el puerto: " + PORT));
